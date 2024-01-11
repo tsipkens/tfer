71292068cdd939dc5778e8e732195aa0a0331b5d
Timothy Sipkens <tsipkens@uwaterloo.ca>
2024-01-11 16:11:24 -0500

    Update prop_dma.m

--- prop_dma.m
+++ prop_dma.m
@@ -3,52 +3,90 @@
 % Author:   Timothy Sipkens, 2019-07-17
 %=========================================================================%
 
-function [prop] = prop_dma(opts)
+function [prop] = prop_dma(varargin)
 
 %-- Parse inputs ---------------------------------------------------------%
-if ~exist('opts','var'); opts = []; end
-if ~isstruct(opts); t0 = opts; opts = struct(); opts.param = t0; end
+if isempty(varargin); opts = struct(); opts.param = '';
+elseif length(varargin) == 1
+    opts = varargin{1};
+    if ~isstruct(opts)
+        t0 = opts;
+        opts = struct();
+        opts.param = t0;  % then assign supplied input to 'param' field
+    end
+elseif length(varargin) == 2
+    opts.param = '';
+    opts.model = varargin{1};
+    opts.flow = varargin{2};
+else; error('Too many inputs.')
+end
+
+% Parameter specs. Default is first option.
+if ~isfield(opts, 'param'); opts.param = ''; end
+switch opts.param
+    case '' % Do nothing.
+    case 'olfert'
+        opts.model = '3018';
+        opts.flow = 'low';
+        
+    case 'buckley'
+        opts.model = '3081';
+        opts.flow = 'low';  % custom flow rates in original
+        
+    otherwise
+        if ~isempty(opts.param)
+            opts.model = opts.param;  % by default, assume param is just the model number
+        end
+end
+
+% Otherwise assign defaults.
+if ~isfield(opts, 'model'); opts.model = ''; end
+if isempty(opts.model); opts.model = '3018'; end  % default is 3018
+prop.model = opts.model;
+
+if ~isfield(opts, 'flow'); opts.flow = ''; end
+if isempty(opts.flow); opts.flow = 'low'; end  % default is low flow
+prop.flow = opts.flow;
+
 if ~isfield(opts,'solver'); opts.solver = 'fullydeveloped'; end % if order not specified
-if ~isfield(opts,'params'); opts.params = 'Olfert'; end % if order not specified
+prop.solver = opts.solver;
+
+% Default temperature and pressure. 
+prop.T = 293; % default temperature [K]
+prop.p = 1;
 
 
-switch opts.params
-    
-    %-- DMA parameters from Olfert lab (TSI 3081) ----------------------------%
-    %-- +FlareNet 2018
-    case {'Olfert','FlareNet18',' Electrostatic Classifier Model 3018'}
+%-- Assign classifier dimensions -----------------------------------------%
+switch prop.model
+    case {'3018', '3081', '3080'}  % DMA properties from Olfert/Buckley
+        prop.L = 0.44369; % length, m
+        prop.R1 = 0.00937; % inner radius, m
+        prop.R2 = 0.01961; % outer radius, m
+
+    case 'custom'
+        prop = opts.prop; % read in from opts
+end
+
+
+%-- Assign flow rates ----------------------------------------------------%
+switch prop.flow
+    case 'low'
         prop.Q_s = 0.3/60/1000; % Sample flow [m^3/s]
         prop.Q_a = 0.3/60/1000; % Aerosol flow [m^3/s]
         prop.Q_c = 3/60/1000; % Sheath flow [m^3/s]
         prop.Q_m = 3/60/1000; % Exhaust flow [m^3/s]
-        prop.L = 0.44369; % length, m
-        prop.R1 = 0.00937; % inner radius, m
-        prop.R2 = 0.01961; % outer radius, m
-        prop.T = 293; % temperature, K
-        prop.p = 1;
-        
-    %-- DMA parameters from Buckley et al. (TSI 3081) ------------------------%
-    case {'Buckley'}
+
+    case 'high'
+        prop.Q_s = 1.5/60/1000;
+        prop.Q_a = 1.5/60/1000;
+        prop.Q_c = 15/60/1000;
+        prop.Q_m = 15/60/1000;
+
+    case 'buckley'
         prop.Q_c = 4.89E-3/60; % sheath flow [m^3/s]
         prop.Q_a = 1.02E-3/60; % aerosol flow [m^3/s]
         prop.Q_s = Q_a; % sample flow [m^3/s]
         prop.Q_m = Q_c; % exhaust flow [m^3/s]
-        prop.L = 0.4444; % length of chamber [m]
-        prop.R2 = 0.01961; % outer electrode radius [m]
-        prop.R1 = 0.00937; % inner electrode radius [m]
-        prop.T = 294; % temperature [K]
-        prop.p = 1;
-        
-    case {' Electrostatic Classifier Model 3080'}
-        prop.L = 0.44369; % length of chamber [m]
-        prop.R2 = 0.00937; % outer electrode radius [m]
-        prop.R1 = 0.01961; % inner electrode radius [m]
-        prop.T = 293; % default temperature [K]
-        prop.p = 1;
-
-    case 'custom'
-        prop = opts.prop; % read in from opts
-        
 end
 
 
@@ -60,6 +98,7 @@ prop.Rd = 1 / prop.bet;  % compute theoretical resolution
 gam = (prop.R1/prop.R2)^2;
 kap = prop.L/prop.R2; % Stolzenburg Manuscript Eqn 9
 % kap = L*R2/(R2^2-R1^2); % Buckey et al.
+
 
 %-- Calculate G_DMA ------------------------------------------------------%
 switch opts.solver
@@ -102,7 +141,6 @@ switch opts.solver
         return;
 
 end
-
 
 end
 
