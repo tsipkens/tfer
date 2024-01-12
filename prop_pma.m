b8cb4be623b761be630565b355f526d4650c51b4
Timothy Sipkens <tsipkens@uwaterloo.ca>
2024-01-12 15:07:34 -0500

    Convert prop_pma to default to soot

--- prop_pma.m
+++ prop_pma.m
@@ -20,12 +20,13 @@ prop = load_spec(spec);
 
 % --If Dm not set, assign one now ----------------------------------------%
 if ~isfield(prop, 'Dm')
-    prop.rho0 = pi*1000/6; % ~524;
-    prop.Dm = 3;
+    % Default is soot. 
+    prop.rho0 = 0.0612;
+    prop.Dm = 2.48;
     
-    %-- For soot --%
-    % prop.rho0 = 0.0612;
-    % prop.Dm = 2.48;
+    % Water spheres. 
+    % prop.rho0 = pi*1000/6; % ~524;
+    % prop.Dm = 3;
 end
 
 
